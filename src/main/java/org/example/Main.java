7091b410e99e6888c327a528f163550ce6f18c6c
Ruslan Sakaev <hardist@mail.ru>
2023-07-30 15:39:13 +0300

    Вызов метода с ошибками

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import exceptions.CustomerException;
 import exceptions.ProductException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ProductException, AmountException {
         // Создание массива покупателей (инициализация 2 элементами)
         Buyer[] buyers = new Buyer[2];
         buyers[0] = Buyer.createBuyer("Иванов Иван", 30, "+7 123-456-7890");
@@ -22,22 +22,55 @@ public class Main {
         // Создание массива заказов (пустой на 5 элементов)
         Order[] orders = new Order[5];
 
-        // Пример создания заказа с помощью статического метода makePurchase
+        // Пример вызова метода совершения покупки несколько раз с обработкой исключений в заданном порядке
         int quantity = 3;
         try {
-            orders[0] = Order.makePurchase(buyers[0], products[1], quantity);
-        } catch (CustomerException | ProductException | AmountException e) {
+            // Передаем неверный товар, должно вывести сообщение об ошибке
+            orders[0] = Order.makePurchase(buyers[0], null, quantity);
+        } catch (ProductException e) {
             System.out.println("Ошибка при создании заказа: " + e.getMessage());
-            e.printStackTrace();
+        } catch (AmountException | CustomerException e) {
+            // Ничего не делаем, так как это не ошибка связанная с неверным товаром
         }
 
-        // Пример создания еще одного заказа
-        quantity = -5; // Пример передачи недопустимого количества товара
+        // Пример создания еще одного заказа с неверным количеством
+        quantity = -5; // Передаем недопустимое количество товара
         try {
             orders[1] = Order.makePurchase(buyers[1], products[3], quantity);
-        } catch (CustomerException | ProductException | AmountException e) {
+        } catch (ProductException e) {
             System.out.println("Ошибка при создании заказа: " + e.getMessage());
-            e.printStackTrace();
+        } catch (AmountException e) {
+            System.out.println("Ошибка при создании заказа: " + e.getMessage() + ". Покупка будет совершена в количестве 1.");
+            try {
+                // Покупка в количестве 1, так как было передано неверное количество
+                orders[1] = Order.makePurchase(buyers[1], products[3], 1);
+            } catch (CustomerException ex) {
+                System.out.println("Ошибка при создании заказа: " + ex.getMessage());
+                System.exit(1); // Выход из программы с кодом ошибки 1
+            }
+        } catch (CustomerException e) {
+            System.out.println("Ошибка при создании заказа: " + e.getMessage());
+            System.exit(1); // Выход из программы с кодом ошибки 1
+        }
+
+        // Пример создания еще одного заказа с неверным пользователем
+        quantity = 2;
+        try {
+            // Передаем неверного пользователя, должно вызвать исключение
+            orders[2] = Order.makePurchase(null, products[1], quantity);
+        } catch (ProductException | AmountException e) {
+            System.out.println("Ошибка при создании заказа: " + e.getMessage());
+        } catch (CustomerException e) {
+            System.out.println("Ошибка при создании заказа: " + e.getMessage());
+            System.exit(1); // Выход из программы с кодом ошибки 1
+        }
+
+        // Пример создания еще одного заказа с верными данными
+        quantity = 2;
+        try {
+            orders[3] = Order.makePurchase(buyers[0], products[1], quantity);
+        } catch (ProductException | AmountException | CustomerException e) {
+            // Ничего не делаем, так как это не ошибка связанная с неверным товаром, количеством или пользователем
         }
 
         // Пример вывода информации о заказах
@@ -51,5 +84,16 @@ public class Main {
                 System.out.println("-----------------------------");
             }
         }
+        // Подсчет и вывод итогового количества совершенных покупок
+        int totalPurchases = 0;
+        for (Order order : orders) {
+            if (order != null) {
+                totalPurchases++;
+            }
+        }
+        System.out.println("Итоговое количество совершенных покупок: " + totalPurchases);
     }
 }
+
+
+
